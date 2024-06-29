be101c3664abdaf98008f59df4b30029e899a6c0
Hiago Dias <51371403+HiagoDiaaas@users.noreply.github.com>
2024-06-28 19:42:44 -0500

    refactor

--- src/Problem1/Prog1.java
+++ src/Problem1/Prog1.java
@@ -1,18 +1,18 @@
 package Problem1;
 
 public class Prog1 {
-    public static void displayDigitsReverse(int n) {
+    public static void reverseInteger(int n) {
         if (n < 10) {
             System.out.println(n);
         } else {
             System.out.print(n % 10);
-            displayDigitsReverse(n / 10);
+            reverseInteger(n / 10);
         }
     }
 
     public static void main(String[] args) {
         int number = 12345;
         System.out.println("Digits in reverse order:");
-        displayDigitsReverse(number);
+        reverseInteger(number);
     }
 }
